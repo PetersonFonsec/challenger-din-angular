efab4149902410ad630a5b03df9e140912cb8141
Peterson Fonseca <petersonsimiao1@gmail.com>
2020-04-25 01:27:28 -0300

    adicionado icones ao icomoon

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderModule } from './components/header/header.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
