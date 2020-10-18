3368c061795175be170d779ad03bb7f12a49b9e4
Peterson <petersonsimiao1@gmail.com>
2020-10-18 17:54:10 -0300

    feat: montado header

--- pokedex/src/app/pages/list/list.module.ts
+++ pokedex/src/app/pages/list/list.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list.component';
 import { ListRoutingModule } from './list-routing.module';
+import { HeaderModule } from 'src/app/components/header/header.module';
 
 @NgModule({
   declarations: [ListComponent],
-  imports: [CommonModule, ListRoutingModule],
+  imports: [CommonModule, ListRoutingModule, HeaderModule],
 })
 export class ListModule {}
