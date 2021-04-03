a1f4babe723b7316a7d93b9676565ac2f0e35073
Peterson <petersonsimiao1@gmail.com>
2021-04-03 18:52:21 -0300

    feat: implementado lazy-loading nos cads

--- challenger-din-angular/src/app/components/cards/card-info/card-info.module.ts
+++ challenger-din-angular/src/app/components/cards/card-info/card-info.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardInfoComponent } from './card-info.component';
 import { ButtonModule } from '../../button/button.module';
+import { LazyLoadingImageModule } from 'src/app/directives/lazy-loading-image/lazy-loading-image.module';
 
 @NgModule({
   declarations: [CardInfoComponent],
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, LazyLoadingImageModule],
   exports: [CardInfoComponent],
 })
 export class CardInfoModule {}
